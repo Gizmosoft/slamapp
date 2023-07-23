f8bb398cfd142c24289298c608a19f174ebadb47
Amritesh Anand <amriteshanand7@gmail.com>
2023-07-23 12:49:40 +0530

    Refactor the code using Lombok

--- slamapp/src/main/java/com/slam/slamapp/model/User.java
+++ slamapp/src/main/java/com/slam/slamapp/model/User.java
@@ -3,7 +3,14 @@ package com.slam.slamapp.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "userdata")
 public class User {
@@ -13,27 +20,27 @@ public class User {
     private String name;
     private String email;
 
-    public String getId() {
-        return id;
-    }
+    // public String getId() {
+    //     return id;
+    // }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    // public void setId(String id) {
+    //     this.id = id;
+    // }
 
-    public String getName() {
-        return name;
-    }
+    // public String getName() {
+    //     return name;
+    // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
 
-    public String getEmail() {
-        return email;
-    }
+    // public String getEmail() {
+    //     return email;
+    // }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // public void setEmail(String email) {
+    //     this.email = email;
+    // }
 }
