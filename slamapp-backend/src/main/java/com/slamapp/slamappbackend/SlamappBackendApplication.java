5b291d9de4b7f03e030487ba549e49b1d721aa45
kartikeyhebbar <vijayakumarhebbar.k@northeastern.edu>
2023-12-16 12:36:59 -0500

    initialized frontend and backend projects

---
+++ slamapp-backend/src/main/java/com/slamapp/slamappbackend/SlamappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.slamapp.slamappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlamappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlamappBackendApplication.class, args);
+	}
+
+}
